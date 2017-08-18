204c67b8fcba01da6999ea2ae6aa070145e4bbf3
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-08-18 12:14:45 -0500

    add methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { AppService } from './app.service'
 })
 export class AppComponent implements OnInit{
   items: Array<any>;
+  model: any = {};
 
   constructor(private _service: AppService) {}
 
@@ -16,6 +17,50 @@ export class AppComponent implements OnInit{
     this._service.getItems()
       .subscribe(
         items => this.items = items,
+        error =>  console.log(error));
+  }
+
+  getItemById(id: number) {
+    this._service.getItemById(id)
+      .subscribe(
+        item => this.model = item,
+        error =>  console.log(error));
+  }
+
+  createItem() {
+    this._service.createItem(this.model)
+      .subscribe(
+        item  => {
+          item.code = this.model.code;
+          item.name = this.model.name;
+          this.items.push(item);
+          this.model = {};
+          console.log('item');
+          console.log(item);
+        },
+        error =>  console.log(error));
+  }
+
+  updateItem() {
+    this._service.updateItem(this.model)
+      .subscribe(
+        item  => {
+          this.model = {};
+          console.log('item');
+          console.log(item);
+        },
+        error =>  console.log(error));
+  }
+
+  deleteItem(item: any) {
+    let indexItem = this.items.indexOf(item);
+    this._service.deleteItem(item.itemId)
+      .subscribe(
+        item => {
+          console.log('item');
+          console.log(item);
+          this.items.splice(indexItem, 1);
+        },
         error =>  console.log(error));
   }
 
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,14 +7,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AppService {
   API_URL: String;
+  token: string;
 
   constructor(private http: Http) {
     this.API_URL = 'http://localhost:5000/api/v1';
+    this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwibmFtZSI6ImFkbWluaXN0cmFkb3IiLCJleHAiOjE1MDMxNTM1ODksImlhdCI6MTUwMzA2NzE4OX0.OzMDr9SAdc1fQUQ_-qw_RzjeAgfF5XP-yOTi0V7IU08';
   }
 
   getItems(): Observable<any[]> {
-    // let headers = new Headers({ 'Authorization': `Bearer ${this.authenticationService.token}` });
-    let headers = new Headers({ 'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwibmFtZSI6ImFkbWluaXN0cmFkb3IiLCJleHAiOjE1MDMxNTM1ODksImlhdCI6MTUwMzA2NzE4OX0.OzMDr9SAdc1fQUQ_-qw_RzjeAgfF5XP-yOTi0V7IU08` });
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
     let options = new RequestOptions({ headers: headers });
 
     return this.http.get(`${this.API_URL}/items/`, options)
@@ -22,21 +23,39 @@ export class AppService {
       .catch(this.handleError);
   }
 
-  createItem(data: any): Observable<any> {
-    let headers = new Headers({ 'Authorization': `Bearer ` });
+  getItemById(id: number): Observable<any[]> {
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
     let options = new RequestOptions({ headers: headers });
 
-    return this.http.post(`${this.API_URL}api/user`, { data }, options)
-      .map((response: Response) => response.json().user)
+    return this.http.get(`${this.API_URL}/items/${id}`, options)
+      .map((response: Response) => response.json())
+      .catch(this.handleError);
+  }
+
+  createItem(data: any): Observable<any> {
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(`${this.API_URL}/items/`, { data }, options)
+      .map((response: Response) => response.json())
       .catch(this.handleError);
   }
 
   updateItem(data: any): Observable<any> {
-    let headers = new Headers({ 'Authorization': `Bearer ` });
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
     let options = new RequestOptions({ headers: headers });
 
-    return this.http.put(`${this.API_URL}api/user/${data._id}`, { data }, options)
-      .map((response: Response) => response.json().user)
+    return this.http.put(`${this.API_URL}/items/${data.itemId}`, { data }, options)
+      .map((response: Response) => response.json())
+      .catch(this.handleError);
+  }
+
+  deleteItem(id: number): Observable<any[]> {
+    let headers = new Headers({ 'Authorization': `Bearer ${this.token}` });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.delete(`${this.API_URL}/items/${id}`, options)
+      .map((response: Response) => response.json())
       .catch(this.handleError);
   }
 
