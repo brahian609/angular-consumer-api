3dca27412259f14a34f0fe6a6ca8e39313e094c3
Brahian Giraldo Medina <brahian609@hotmail.com>
2017-08-18 11:08:08 -0500

    implement service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+  items: Array<any>;
+
+  constructor(private _service: AppService) {}
+
+  ngOnInit() {
+    this._service.getItems()
+      .subscribe(
+        items => this.items = items,
+        error =>  console.log(error));
+  }
+
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AppService {
+  API_URL: String;
+
+  constructor(private http: Http) {
+    this.API_URL = 'http://localhost:5000/api/v1';
+  }
+
+  getItems(): Observable<any[]> {
+    // let headers = new Headers({ 'Authorization': `Bearer ${this.authenticationService.token}` });
+    let headers = new Headers({ 'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwibmFtZSI6ImFkbWluaXN0cmFkb3IiLCJleHAiOjE1MDMxNTM1ODksImlhdCI6MTUwMzA2NzE4OX0.OzMDr9SAdc1fQUQ_-qw_RzjeAgfF5XP-yOTi0V7IU08` });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.get(`${this.API_URL}/items/`, options)
+      .map((response: Response) => response.json().data)
+      .catch(this.handleError);
+  }
+
+  createItem(data: any): Observable<any> {
+    let headers = new Headers({ 'Authorization': `Bearer ` });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(`${this.API_URL}api/user`, { data }, options)
+      .map((response: Response) => response.json().user)
+      .catch(this.handleError);
+  }
+
+  updateItem(data: any): Observable<any> {
+    let headers = new Headers({ 'Authorization': `Bearer ` });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.put(`${this.API_URL}api/user/${data._id}`, { data }, options)
+      .map((response: Response) => response.json().user)
+      .catch(this.handleError);
+  }
+
+  private handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+}
